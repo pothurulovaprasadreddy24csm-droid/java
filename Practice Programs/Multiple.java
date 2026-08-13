322ca3fff5b48799cdb6910f09c62f557b49d4fd
pothurulovaprasadreddy24csm-droid <pothurulovaprasadreddy.24.csm@anits.edu.in>
2026-08-13 20:42:22 +0530

    Create Multiple.java

---
+++ Practice Programs/Multiple.java
@@ -0,0 +1,29 @@
+package opp;
+
+interface A {
+    void showA();
+}
+
+interface B {
+    void showB();
+}
+
+class C implements A, B {
+
+    public void showA() {
+        System.out.println("A");
+    }
+
+    public void showB() {
+        System.out.println("B");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        C obj = new C();
+
+        obj.showA();
+        obj.showB();
+    }
+}
